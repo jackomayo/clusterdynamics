0d0ed023d856c5ace7bd460a25b9f0773cdc205e
jmayo <jackomayo@gmail.com>
2015-03-09 02:36:14 +0000

    initial commint, basic outline

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Mar 9, 2015
+ *      Author: jmayo
+ */
+
+//Initial r0 M0 Trlx Tcross
+//Trlx & Tcross functions of r(T) M(T)
+//setup inital mass function Chabrier
+//time step T+deltaT
+
+
